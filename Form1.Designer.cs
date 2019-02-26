093957a5f5fd3dcc737311f076c726aa74c98220
Nattttt <natreyes12@hotmail.com>
2019-02-26 14:42:40 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace convertidorFormatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(268, 193);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Convertir !";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cargar Archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(357, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "examinar...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 319);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspose.Slides;
+using Aspose.Slides.Export;
+using Spire.Doc;
+using Spire.Xls;
+using Spire.Doc.Documents;
+using System.IO;
+
+namespace convertidorFormatos
+{
+    public partial class Form1 : Form
+    {
+
+        string ruta;
+        string rutaout;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op1 = new OpenFileDialog();
+
+            if (op1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ruta = op1.FileName;
+                label2.Text = Path.GetFileName(ruta).ToString();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            SaveFileDialog op2 = new SaveFileDialog();
+
+            if (op2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                rutaout = op2.FileName;
+                label1.Text = Path.GetFileName(rutaout).ToString();
+            }
+
+
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+
+
+            // The path to the documents directory.
+            
+
+            // Open the ODP file
+           // Presentation pres = new Presentation(ruta);
+            
+            // Saving the ODP presentation to PPTX format
+           // pres.Save(rutaout + "AccessOpenDoc_out.pptx", SaveFormat.Pptx);
+
+
+            
+
+             
+            Workbook workbook = new Workbook();
+            workbook.LoadFromFile(ruta);
+            workbook.SaveToFile(rutaout +"Result.ods", Spire.Xls.FileFormat.ODS);
+           
+           /*  Document doc = new Document();
+             doc.LoadFromFile(ruta);
+             doc.SaveToFile(rutaout +"output.odt", Spire.Doc.FileFormat.Odt);
+
+            /*
+            Document doc = new Document();
+            doc.LoadFromFile("SampleODTFile.odt");
+            doc.SaveToFile(rutaout +"output.docx", FileFormat.Docx);
+            
+             //convertir excel
+
+             Workbook workbook = new Workbook();
+            workbook.LoadFromFile("Sample.xlsx");
+            workbook.SaveToFile("Result.ods",FileFormat.ODS);
+             
+             */
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+     
+    }
+}
